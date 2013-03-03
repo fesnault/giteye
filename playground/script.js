05d036adb9d44618c615c94d5be73ae70e1e8f94
Esnault Frederic <esnault.frederic@gmail.com>
2013-03-03 23:04:01 +0100

    Included a d3 js playground directory containing static data to play withn an index.html, and a script.js to mess around with, and try to achieve a nice graph. Currently the graph is redrawn every half second, removing a commit for each iteration. Graph is scaled to fill up the height.

---
+++ playground/script.js
@@ -0,0 +1,167 @@
+var min=0, max=0,width=960,height=500,margin=10,padding=100,
+svg=d3.select("#workplace")
+.append("svg")
+.attr("width",width)
+.attr("height",height)
+.style("border", "solid black 1px");
+ 
+// The data join should be done on the commits ids.
+function key(d) {
+	return d.id;
+}
+
+function formatMillisecondDate(d) {
+	var format = d3.time.format("%Y, %b %d");
+	if (d instanceof Date) {
+		return format(d);
+	}
+	return format(new Date(d));
+}
+
+var delay = 500;
+
+setInterval(updateData, 1000);
+
+var repository;
+var currentCommits;
+var originalCommits;
+// scalers - scales the commits dates base on the idea that they are displayed vertically. So the scale range uses the height as a max.
+// X is scaled by the number of branches (from 1 to 10) - TODO : make it dynamic from the branch count in the data.
+var x = d3.scale.linear().domain([0,50]).range([padding+margin,width-margin]);
+// Y is scaled by the commit date
+//var y = d3.scale.linear().domain([min, max]).range([height-margin, margin]);
+var y = d3.time.scale.utc().domain([min, max]).range([height-margin, margin]);
+
+
+function updateData() {
+	currentCommits = currentCommits.slice(0, currentCommits.length-1);
+	redraw(currentCommits);
+}
+
+d3.json("data.json", function(jrep) {
+	repository = jrep;
+	originalCommits = repository.commits;
+	currentCommits = originalCommits;
+	redraw(currentCommits);
+});
+
+var yAxis = d3.svg.axis()
+	.scale(y)
+	.orient("left")
+	.ticks(10)
+	.tickFormat(formatMillisecondDate);
+var axisElement = svg.append("g")
+	    .attr("class", "axis");
+	    //.attr("transform", "translate(" + 80 + ",0)");
+
+function redraw(commits) {
+
+	var max = d3.max(commits,
+            function (d) {
+                return d.date;
+            }
+    );
+    var min = d3.min(commits,
+            function (d) {
+                return d.date;
+            }
+    );
+	y = d3.time.scale.utc().domain([min, max]).range([height-margin, margin]);
+	
+
+
+    // Axis
+    //yAxis.scale(y);
+    //axisElement.call(yAxis);
+
+
+	var nodes = [];
+    var links = [];
+    commits.forEach(function(d, i) {
+        var source = {"x": x(d.lane), "y": y(+d.date)};
+        nodes.push(d);
+        if (d.parents !== null) {
+	        d.parents.forEach(function(c, i) {
+	        	var target = {"x": x(c.lane), "y": y(+c.date)};
+	            links.push({"source": source, "target": target});
+	        });
+    	}
+    });
+    //var line = d3.svg.line()
+    //	.attr("x1", function(d) {return d.source.x; })
+    //	.attr("y1", function(d) {return d.source.y; })
+    //	.attr("x2", function(d) {return d.target.x; })
+    //	.attr("y2", function(d) {return d.target.y; })
+
+    var diagonal = d3.svg.diagonal().projection(function(d) {
+    	return [d.x, d.y]; 
+    });
+	
+	var linksSelection = svg.selectAll(".link").data(links);
+	var linksEnterSelection = linksSelection.enter();
+	var linksExitingSelection = linksSelection.exit();
+
+    var enteringLinks = linksEnterSelection.append("path")
+        .attr("class", "link")
+        .attr("d", diagonal)
+        .style("stroke", "#99AAFF")
+        .style("stroke-width", "2px")
+        .style("fill", "#FFFFFF");
+
+	// Create a selection of circles for commits
+	var commitsSelection = svg.selectAll(".circle").data(nodes, key);
+	var commitsEnterSelection = commitsSelection.enter().append("g").attr("class", "circle");
+	var commitsExitingSelection = commitsSelection.exit();
+
+	// Create a circle for each commit and set it in the top left angle
+	var enteringCommits = commitsEnterSelection.append("circle")
+			.attr("id", function(d) {return d.id;})
+			.attr("cx", function(d) {return x(d.lane);})
+			.attr("cy", function(d) {return y(+d.date);})
+			.attr("r", 4)
+			.style("fill", "#FFFFFF")
+			.style("stroke", "#8888FF")
+			.style("stroke-width", "2px");
+
+	enteringLinks.transition()
+      .duration(delay)
+      .style("opacity", 1);
+
+    enteringCommits.transition()
+      .duration(delay)
+      .style("opacity", 1);
+
+    commitsExitingSelection.transition()
+      .duration(delay)
+      .style("opacity", 1e-6)
+      .remove();
+
+    commitsSelection
+		.transition()
+        .duration(delay)
+        .style("opacity", 1);
+
+    var t = svg.transition()
+		.duration(delay);
+		
+		 
+	t.selectAll("circle")
+		.attr("cy", function(d) { return y(+d.date); });
+
+	t.selectAll("path").attr("d", diagonal);
+
+	
+    linksExitingSelection.transition()
+      .duration(delay)
+      .style("opacity", 1e-6)
+      .remove();
+
+
+
+	
+
+	// transition function
+	//svg.selectAll("circle").transition().duration(1000)
+    //		.attr("cx",)
+    //		.attr("cy",);
+}
