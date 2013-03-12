75763b22558cde6d143528686bfafa15ed4dcf6f
Esnault Frederic <esnault.frederic@gmail.com>
2013-03-12 23:31:28 +0100

    Managed to create a mouse wheel panning. Not very fluid, but working.

--- playground/script.js
+++ playground/script.js
@@ -1,10 +1,62 @@
-var min=0, max=0, previousMin=0, previousMax=0, commitPadding=4,width=1300,height=500,margin=10,padding=100,bgcolor="#FFFFFF"
-svg=d3.select("#svgcontainer")
+var min=0, max=0, previousMin=0, previousMax=0, commitPadding=4,width=1300,height=500,margin=10,padding=100,bgcolor="#FFFFFF",
+ypan=0;
+// scalers - scales the commits dates base on the idea that they are displayed vertically. So the scale range uses the height as a max.
+// X is scaled by the number of branches (from 1 to 10) - TODO : make it dynamic from the branch count in the data.
+var x = d3.scale.linear().domain([0,25]).range([margin,width-margin]);
+// Y is scaled by the commit date
+//var y = d3.scale.linear().domain([min, max]).range([height-margin, margin]);
+var maxPaddingHeight = 0;
+var minPaddingHeight = 0;
+var originalTranslate = 0;
+var currentPan = 0;
+var y = d3.scale.linear().domain([min, max]).range([margin, height-margin]);
+var svg=d3.select("#svgcontainer")
 .append("svg").attr("class", "workspace");
 
-var svgGroup = svg.insert("g");
+
+  
+//.on("mousedown", mousedown)
+//.on("mousemove", mousemove)
+//.on("mouseup", mouseup)
+
+
+function doit(d) {
+  console.log(d);
+}
+
+var svgGroup = svg.insert("g").attr("class", "log");
+  
 var linksGroup = svgGroup.insert("g").attr("class", "links-group");
 var commitsGroup = svgGroup.insert("g").attr("class", "commits-group");
+var origin;
+
+
+function mousedown() {
+  origin = d3.mouse(svgGroup.node());
+}
+
+function mousemove() {
+      if (!origin) { return; }
+        var m = d3.mouse(svgGroup.node());
+        ypan = ypan + (m[1]-origin[1]);
+        convertedPan = y.invert(ypan);
+        maxPaddingHeight = (max*commitPadding) - y.invert(height-4*margin);
+        if (convertedPan >= 0) { ypan = y(0); }
+        if (convertedPan <= -( maxPaddingHeight )) { ypan = -(y( maxPaddingHeight )); }
+        d3.select(".log").attr("transform", "translate(0,"+ypan+")");
+          //dragged.x = x.invert(Math.max(0, Math.min(width, m[0])));
+          //dragged.y = y.invert(Math.max(0, Math.min(height, m[1])));
+          //d3.select(".log").attr("transform", "translate(0,"+ypan+")");
+       // update();
+    }
+ 
+    function mouseup() {
+      if (!origin) { return; }
+        mousemove();
+        origin = null;
+    }
+
+
  
 // The data join should be done on the commits ids.
 
@@ -27,14 +79,9 @@ var delay = 500;
 var repository;
 var currentCommits;
 var originalCommits;
-// scalers - scales the commits dates base on the idea that they are displayed vertically. So the scale range uses the height as a max.
-// X is scaled by the number of branches (from 1 to 10) - TODO : make it dynamic from the branch count in the data.
-var x = d3.scale.linear().domain([0,25]).range([margin,width-margin]);
-// Y is scaled by the commit date
-//var y = d3.scale.linear().domain([min, max]).range([height-margin, margin]);
-var y = d3.scale.linear().domain([min, max]).range([margin, height-margin]);
 
-var line = d3.svg.diagonal().projection( function (d) { return [d.x, d.y*commitPadding] } );
+
+var line = d3.svg.diagonal().projection( function (d) { return [d.x, d.y] } );
 var initline = d3.svg.diagonal().projection( function (d) {
   if (d.y < min) {
     return [d.x, margin];
@@ -45,28 +92,28 @@ var initline = d3.svg.diagonal().projection( function (d) {
 );
 
 // buttons
-var resetButton = d3.select("svg").append("g").attr("class","button reset");
-resetButton.append("circle")
-      .attr("cx", 1000)
-      .attr("cy", 200)
-      .attr("r", 30)
-      .on("click", resetZoom);
-resetButton.append("text")
-        .attr("x", 1000)
-        .attr("y", 200)
-        .text("Reset")
-        .on("click", resetZoom);
-var zoomButton = d3.select("svg").append("g").attr("class","button zoom");
-zoomButton.append("circle")
-      .attr("cx", 1000)
-      .attr("cy", 270)
-      .attr("r", 30)
-      .on("click", initBrush);
-zoomButton.append("text")
-        .attr("x", 1000)
-        .attr("y", 270)
-        .text("Zoom")
-        .on("click", initBrush);
+//var resetButton = d3.select("svg").append("g").attr("class","button reset");
+//resetButton.append("circle")
+//      .attr("cx", 1000)
+//      .attr("cy", 200)
+//      .attr("r", 30)
+//      .on("click", resetZoom);
+//resetButton.append("text")
+//        .attr("x", 1000)
+//        .attr("y", 200)
+//        .text("Reset")
+//        .on("click", resetZoom);
+//var zoomButton = d3.select("svg").append("g").attr("class","button zoom");
+//zoomButton.append("circle")
+//      .attr("cx", 1000)
+//      .attr("cy", 270)
+//      .attr("r", 30)
+//      .on("click", initBrush);
+//zoomButton.append("text")
+//        .attr("x", 1000)
+//        .attr("y", 270)
+//        .text("Zoom")
+//        .on("click", initBrush);
 
 function updateData() {
 	if (currentCommits.length > 50) {
@@ -79,6 +126,11 @@ d3.json("data.json", function(jrep) {
 	repository = jrep;
 	originalCommits = repository.commits;
 	currentCommits = originalCommits;
+  max = d3.max(currentCommits, function(d) { return d.position;});
+  min = d3.min(currentCommits, function(d) { return d.position;});
+  maxPaddingHeight = (((height-margin)*commitPadding)-height)+2*margin;
+  minPaddingHeight = 0;
+  y = d3.scale.linear().domain([min, max]).range([margin, (height-margin)*commitPadding]);
 	redraw(currentCommits);
 });
 
@@ -149,7 +201,46 @@ function brushend() {
   }
 }
 
-function zoom() {
+
+
+var zoom = d3.behavior.zoom()
+  .on('zoom', function() {
+      var currentTranslate = zoom.translate()[1];
+      var panDirection = 1;
+      if (currentTranslate - originalTranslate < 0) {
+        panDirection = -1;
+      } 
+      
+      var panFactor = 20;
+      originalTranslate = currentTranslate;
+      currentPan = currentPan+panDirection*panFactor;
+      if (currentPan > maxPaddingHeight) { currentPan = maxPaddingHeight; }
+      else if (currentPan < minPaddingHeight) { currentPan = minPaddingHeight; }
+      svgGroup.attr("transform", "translate(0, " + -currentPan + ")");
+  });
+svg.call(zoom);
+//svg.call(d3.behavior.zoom().scaleExtent([1,1])
+//      .on("zoom", function() {
+        
+        //convertedPan = y.invert(ypan);
+        //var maxPaddingHeight = (max*commitPadding) - y.invert(height-4*margin);
+        //  if (convertedPan >= 0) { ypan = y(0); }
+        //  if (convertedPan <= -( maxPaddingHeight )) { ypan = -(y( maxPaddingHeight )); }
+        //d3.select(".log").attr("transform", "translate(0,"+ypan+")");
+        //var currentTranslate = d3.event.translate[1];
+        //var delta = currentTranslate - originalTranslate;
+        //originalTranslate = currentTranslate;
+        //currentPan = currentPan + delta;
+        //var convertedPan = y.invert(currentPan);
+        
+        //if (currentTranslate <= -(maxPaddingHeight)) { currentTranslate = -(maxPaddingHeight); d3.event.translate[1] = y(currentTranslate); }
+//        console.log("ypan : "+ypan);
+        //svgGroup.attr("transform", "translate(0, " + d3.event.translate[1] + ")");
+//      }
+//    )
+//  );
+
+function zoom2() {
   var selectedCommitsSelection = svgGroup.selectAll(".circle.selected")[0];
   if (selectedCommitsSelection.length === 0) { return; }
   var selectedCommits = [];
@@ -169,9 +260,7 @@ function resetZoom() {
 function redraw(commits) {
   previousMax = max;
   previousMin = min;
-	max = d3.max(commits, function(d) { return d.position;});
-  min = d3.min(commits, function(d) { return d.position;});
-	y = d3.scale.linear().domain([min, max]).range([margin, height-margin]);
+	
 
 	var nodes = [];
   var links = [];
@@ -239,10 +328,12 @@ function redraw(commits) {
 
 
   var t = svg.transition().duration(delay);
-	t.selectAll("circle.commit").attr("cy", function(d) { return y(+d.position)*commitPadding; });
+	t.selectAll("circle.commit").attr("cy", function(d) { return y(+d.position); });
 	t.selectAll("path").attr("d", line);
 
   linksExitingSelection.transition().style("opacity", 1e-6).remove();
+
+  
 
 
 }
